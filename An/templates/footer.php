9e644e612c8a6350f4ed8f4efb0278d521221646
Trang <trangtruong16101993@gmail.com>
2019-12-23 16:02:53 +0700

    An

---
+++ An/templates/footer.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>footer</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Styles Source -->
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="debug footer container-fluid">
+<!--    Footer Part 1 Shop Intro     -->
+    <div class="debug row footer1">
+        <div class="debug col-3">
+            logo
+        </div>
+        <div class="debug col-6">
+            <h5 class="header5">ABOUT THE SHOP</h5>
+            <p>Sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio.</p>
+        </div>
+        <div class="debug col-3">
+            <h5 class="header5">FOLLOW US ON SOCIAL</h5>
+            <div class="row social-icon">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa fa-google" aria-hidden="true"></i>
+                <i class="fa fa-youtube" aria-hidden="true"></i>
+                <i class="fa fa-skype" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+    <div class="line"></div>
+<!--    Footer Part 2 Shop Activities   -->
+    <div class="debug row footer2">
+        <div class="debug col-3">
+            <div>
+                <h5 class="header5">OPENNING TIME</h5>
+                <ul class="list-lev1">
+                    <li class="list-lev2">
+                        <span>Monday - Friday</span>
+                        <span class="tab"></span>
+                        <span>8:00 to 20:00</span>
+                    </li>
+                    <li class="list-lev2">
+                        <span>Saturday - Sunday</span>
+                        <span class="tab"></span>
+                        <span>10:00 to 22:00</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-item">
+                <i class="fa fa-phone fa-border" style="font-weight: bold" aria-hidden="true"></i>
+                <span style="font-family: 'Apercu Medium'; font-size: 12px; color: rgba(40,40,40,.9); line-height: 15px">Call us: </span>
+                <span style="font-family: 'Century Gothic'; font-weight: bold; font-size: 20px; line-height: 20px">3999 9993</span>
+            </div>
+        </div>
+        <div class="debug col-3">
+            <h5 class="header5">CATEGORIES</h5>
+            <div>
+                <ul class="list-lev1">
+                    <li class="list-lev2">
+                        <a href="">
+                            Coats/Jackets
+                        </a>
+                    </li>
+                    <li class="list-lev2">
+                        <a href="">
+                            Tops
+                        </a>
+                    </li>
+                    <li class="list-lev2">
+                        <a href="">
+                            Bottoms
+                        </a>
+                    </li>
+                    <li class="list-lev2">
+                        <a href="">
+                            Accessories
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="debug col-3">
+            <h5 class="header5">INFORMATIONS</h5>
+            <div>
+                <ul class="list-lev1">
+                    <li class="list-lev2">
+                        <a href="">
+                            Store Information
+                        </a>
+                    </li>
+                    <li class="list-lev2">
+                        <a href="">
+                            Our Brands
+                        </a>
+                    </li>
+                    <li class="list-lev2">
+                        <a href="">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="debug col-3">
+            <h5 class="header5">CONTACT</h5>
+            <ul class="list-lev1">
+                <li class="list-lev2" style="font-weight: bold">
+                    Warehouse & Offices
+                </li>
+                <li class="list-lev2-item">
+                    <span>7th Floor, A Building, 51 Nguyen Hue Street, Dist. 1, Ho Chi Minh City, Vietnam</span>
+                    <br>
+                    <span>
+                        (+84)28 3899 9983 / (+84)28 3899 9982
+                    </span>
+                </li>
+            </ul>
+            <ul class="list-lev1">
+                <li class="list-lev2"  style="font-weight: bold">
+                    Retail Store
+                </li>
+                <li class="list-lev2-item">
+                    <span>15 Nguyen Hue Street, Dist. 1, Ho Chi Minh City, Vietnam</span>
+                    <br>
+                    <span>
+                        (+84)28 3999 9993 / (+84)28 3999 9992
+                    </span>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="line"></div>
+<!--    Footer Part 3 Copyright Note    -->
+    <div class="debug copyright-footer footer3">
+        © 2019 First Fashion Store.<br>All Rights Reserved.
+    </div>
+</div>
+</body>
+</html>
