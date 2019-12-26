e4a5c56eb36063e808fb5c6ce9f3363731b9ef77
Trang <trangtruong16101993@gmail.com>
2019-12-26 14:46:57 +0700

    home page in CI

---
+++ CodeIgniter/application/views/common/an/templates/footer.php
@@ -0,0 +1,176 @@
+<html lang="en">
+<head>
+    <title>footer</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Local Sources -->
+	<link href="<?php echo base_url('CodeIgniter/An/assets/css/styles.css') ?>" rel="stylesheet">
+	<link href="<?php echo base_url('CodeIgniter/An/assets/css/fontawesome.css') ?>" rel="stylesheet">
+	<script src="<?php echo base_url('CodeIgniter/An/assets/scripts/scripts.js') ?>"></script>
+	<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="debug footer">
+<!--    Footer Part 1 Shop Intro     -->
+    <div class="debug row footer1">
+        <div class="debug col-lg-3 footer-content" style="justify-content: center">
+            logo
+        </div>
+        
+        <div class="debug col-lg-6 footer-content">
+            <h5 class="header5">ABOUT THE SHOP</h5>
+            <p>Sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio.</p>
+        </div>
+        
+        <div class="debug col-lg-3 footer-content">
+            <h5 class="header5">FOLLOW US ON SOCIAL</h5>
+            <div class="row social-icon debug">
+                <a href="">
+                    <i class="fa fa-facebook debug" aria-hidden="true"></i>
+                </a>
+                <a href="">
+                    <i class="fa fa-twitter debug" aria-hidden="true"></i>
+                </a>
+                <a href="">
+                    <i class="fa fa-google debug" aria-hidden="true"></i>
+                </a>
+                <a href="">
+                    <i class="fa fa-youtube debug" aria-hidden="true"></i>
+                </a>
+                <a href="">
+                    <i class="fa fa-skype debug" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="line"></div>
+    <!--    Footer Part 2 Shop Activities   -->
+    <div class="debug row footer2">
+        <div class="debug col-lg-4">
+            <div>
+                <h5 class="header5">OPENNING TIME</h5>
+                <ul class="list-lev1">
+                    <li class="list-lev2">
+                        <span>Monday - Friday</span>
+                        <span class="tab"></span>
+                        <span>8:00 to 20:00</span>
+                    </li>
+                    <li class="list-lev2">
+                        <span>Saturday - Sunday</span>
+                        <span class="tab"></span>
+                        <span>10:00 to 22:00</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="debug footer-item">
+                <div><i class="fa fa-phone fa-call" style="font-weight: bold; font-size: 20px; line-height: 1.8rem; padding-left: 5px" aria-hidden="true"></i></div>
+                <div style="font-family: 'Apercu Medium'; font-size: 15px; color: rgba(40,40,40,.9)">Call us: </div>
+                <div style="font-family: 'Century Gothic'; font-size: 25px; letter-spacing: 2px">3999 9993</div>
+            </div>
+        </div>
+        
+        <div class="debug col-lg-4 row">
+            <div class="debug col-sm-6">
+                <h5 class="header5">CATEGORIES</h5>
+                <div>
+                    <ul class="list-lev1">
+                        <li class="list-lev2 debug">
+                            <a href="" class="debug list-lev1-item">
+                                Coats/Jackets
+                            </a>
+                        </li>
+                        <li class="list-lev2 debug">
+                            <a href="" class="debug list-lev1-item">
+                                Tops
+                            </a>
+                        </li>
+                        <li class="list-lev2 debug">
+                            <a href="" class="debug list-lev1-item">
+                                Bottoms
+                            </a>
+                        </li>
+                        <li class="list-lev2 debug">
+                            <a href="" class="debug list-lev1-item">
+                                Accessories
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="debug col-sm-6">
+
+                <h5 class="header5">INFORMATIONS</h5>
+                <div>
+                    <ul class="list-lev1">
+                        <li class="list-lev2">
+                            <a href="" class="debug list-lev1-item">
+                                Store Information
+                            </a>
+                        </li>
+                        <li class="list-lev2">
+                            <a href="" class="debug list-lev1-item">
+                                Our Brands
+                            </a>
+                        </li>
+                        <li class="list-lev2">
+                            <a href="" class="debug list-lev1-item">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="debug col-lg-4">
+            <h5 class="header5">CONTACT</h5>
+            <ul class="list-lev1 debug">
+                <li class="list-lev2" style="font-weight: bold">
+                    <div class="debug list-lev1-item">Warehouse & Offices</div>
+                </li>
+                <li class="list-lev2-item debug">
+                    <span>7th Floor, A Building, 51 Nguyen Hue Street, Dist. 1, Ho Chi Minh City, Vietnam</span>
+                    <br>
+                    <span>
+                        (+84)28 3899 9983 / (+84)28 3899 9982
+                    </span>
+                </li>
+            </ul>
+            <ul class="list-lev1 debug">
+                <li class="list-lev2"  style="font-weight: bold">
+                    <div class="debug list-lev1-item">Retail Store</div>
+                </li>
+                <li class="list-lev2-item debug">
+                    <span>15 Nguyen Hue Street, Dist. 1, Ho Chi Minh City, Vietnam</span>
+                    <br>
+                    <span>
+                        (+84)28 3999 9993 / (+84)28 3999 9992
+                    </span>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="line"></div>
+<!--    Footer Part 3 Copyright Note    -->
+    <div class="debug copyright-footer footer3">
+        © 2019 First Fashion Store.<br>All Rights Reserved.
+    </div>
+</div>
+</body>
+</html>
---
+++ CodeIgniter/application/views/common/trang/view/contact.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../../../../trang/assets/css/contact.css">
+    <script src="https://kit.fontawesome.com/ba36fe9198.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div id="contact">
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-sm">
+                    <div><a href="https://goo.gl/maps/AeXBFa4uUzb8CZgw7"><i class="fas fa-map-marker-alt"></i></a></div>
+                    24 Phan Liêm, Đa Kao, Quận 1, Hồ Chí Minh, Vietnam
+                </div>
+                <div class="col-sm">
+                    <div><a href="tel:+84 1800 1779"><i class="fas fa-phone-square-alt"></i></a></div>
+                    <div>+84 1800 1779</div>
+                </div>
+                <div class="col-sm">
+                    <div><a href="mailto:aptech2@aprotrain.com"><i class="fas fa-envelope"></i></a></div>
+                    aptech2@aprotrain.com
+                </div>
+            </div>
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.282982943546!2d106.69300631452862!3d10.789624892312572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528cb4ba30aa5%3A0x3a0ddc230888b922!2zMjQgUGhhbiBMacOqbSwgxJBhIEthbywgUXXhuq1uIDEsIEjhu5MgQ2jDrSBNaW5oLCBWaWV0bmFt!5e0!3m2!1sen!2s!4v1576994402735!5m2!1sen!2s" width="1110" height="500" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            </div>
+            <div class="contact-form">
+                <p>GET IN TOUCH</p>
+                <hr>
+                <form action="">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-3">
+                            YOUR NAME
+                            <div><input type="text"></div>
+                            </div>
+                            <div class="col-3">
+                            EMAIL
+                            <div><input type="email"></div>
+                            </div>
+                            <div class="col-3">
+                            SUBJECT
+                            <div><input type="text"></div>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-9">
+                                MESSAGE
+                                <div class="input-mess"><textarea class="userInput"></textarea>
+                                <div><button type="button" class="btn btn-secondary">Secondary</button></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
