b1ae3b1014cb9d08a8d4e94133b9410ae4021560
Trang <trangtruong16101993@gmail.com>
2019-12-27 20:40:33 +0700

    update product page

--- An/README.md
+++
@@ -1,1 +0,0 @@
-e-Project-First-fashion
--- CodeIgniter/application/config/routes.php
+++ CodeIgniter/application/config/routes.php
@@ -52,5 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['/contact'] = 'contact/index';
 $route['/login'] = 'login/index';
+$route['/product'] = 'product/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
